0433297560cd81494db76dc6ffa26bcf0d00065b
Santhosh <santhoshdm@Santhoshs-MacBook-Air.local>
2020-04-29 17:37:45 +0530

    changed ami id

--- app2.tf
+++ app2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app2" {
   instance_type = "t2.micro"
-  ami           = "ami-078951bcd843f606a"
+  ami           = "ami-032cf0cbfd6553e4e"
   key_name      = "aws"
   vpc_security_group_ids = [aws_security_group.sg.id]
   
--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app1" {
   instance_type = "t2.micro"
-  ami           = "ami-078951bcd843f606a"
+  ami           = "ami-032cf0cbfd6553e4e"
   key_name      = "aws"
 
   tags = {
